5dc9e36360ab946ded8b8a5171d7572f5f854fd4
Garri <cibergarri@hotmail.com>
2019-11-27 09:36:17 +0100

    improvement(commitlint): update rules
    match commitlint types with commitizen ones

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,1 +1,19 @@
-module.exports = {extends: ['@commitlint/config-conventional']}
+module.exports = {
+  extends: ['@commitlint/config-conventional'],
+  rules: {
+    'type-enum': [2, 'always', [
+      'feat',
+      'fix',
+      'improvement',
+      'docs',
+      'style',
+      'refactor',
+      'perf',
+      'test',
+      'build',
+      'ci',
+      'chore',
+      'revert',
+    ]],
+  },
+}
